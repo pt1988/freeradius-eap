521bc24975aef00164fef94888b9cd78de3cf3be
Peerapong Thongpubet <pt1988@gmail.com>
2021-11-04 08:44:35 +0000

    add reddb

---
+++ certs/passwords.mk
@@ -0,0 +1,6 @@
+PASSWORD_SERVER	= 'kasetsart2021-output'
+PASSWORD_INNER	= 'whatever'
+PASSWORD_CA	= 'kasetsart2021-output'
+PASSWORD_CLIENT	= 'whatever'
+USER_NAME	= 'user@ku.ac.th'
+CA_DEFAULT_DAYS  = '360'
